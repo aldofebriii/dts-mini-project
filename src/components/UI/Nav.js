e90740f3f15e04098942c54f1bc22d9c565e5dde
aldofebriii <aldothepro@gmail.com>
2022-07-23 11:20:03 +0700

    Update Nav.js

--- src/components/UI/Nav.js
+++ src/components/UI/Nav.js
@@ -67,14 +67,14 @@ export default function NavigationHeader() {
                 </div>
             </div>)}
             {!ctx.isLoggedIn && (
-                    <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-                        <NavLink to="sign-in" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-                            Sign in
-                        </NavLink>
-                        <NavLink to="sign-up" className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-                            Sign up
-                        </NavLink>
-                    </div>
+                <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+                    <NavLink to="sign-in" className="nav-item whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+                        Sign in
+                    </NavLink>
+                    <NavLink to="sign-up" className="nav-item ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                        Sign up
+                    </NavLink>
+                </div>
             )}
         </div>
     </nav>
